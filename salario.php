efe0e2f3629e2536e678d9cd3d2520118d64271a
yazribeiroo <yasmim.rm05@gmail.com>
2025-04-28 20:14:00 -0300

    Update salario.php

--- salario.php
+++ salario.php
@@ -1,222 +1,27 @@
-{
-	"auto_complete":
-	{
-		"selected_items":
-		[
-		]
-	},
-	"buffers":
-	[
-		{
-			"contents": "<?php\n \n$salarioBruto = 7500.00;\n \n \nif ($salarioBruto <= 3036.00) {\n    $aliquota = 0;\n} elseif ($salarioBruto <= 3533.31) {\n    $aliquota = 7.5;\n} elseif ($salarioBruto <= 4688.85) {\n    $aliquota = 15;\n} elseif ($salarioBruto <= 5830.85) {\n    $aliquota = 22.5;\n} else {\n    $aliquota = 27.5;\n}\n \n$valorDesconto = ($salarioBruto * $aliquota) / 100;\n \n$salarioLiquido = $salarioBruto - $valorDesconto;\n \necho \"Salário bruto: R$ \" . number_format($salarioBruto, 2, ',', '.') . \"<br>\";\necho \"Porcentagem de desconto: \" . $aliquota . \"%<br>\";\necho \"Valor do desconto: R$ \" . number_format($valorDesconto, 2, ',', '.') . \"<br>\";\necho \"Salário líquido a receber: R$ \" . number_format($salarioLiquido, 2, ',', '.');\n?>\n \n ",
-			"file": "impRenda.php",
-			"file_size": 748,
-			"file_write_time": 133903547811658707,
-			"settings":
-			{
-				"buffer_size": 718,
-				"encoding": "UTF-8",
-				"line_ending": "Windows"
-			},
-			"undo_stack":
-			[
-				[
-					5,
-					1,
-					"paste",
-					null,
-					"AQAAAAAAAAAAAAAAzgIAAAAAAAAAAAAA",
-					"AQAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPC/"
-				]
-			]
-		}
-	],
-	"build_system": "",
-	"build_system_choices":
-	[
-	],
-	"build_varint": "",
-	"command_palette":
-	{
-		"height": 0.0,
-		"last_filter": "",
-		"selected_items":
-		[
-		],
-		"width": 0.0
-	},
-	"console":
-	{
-		"height": 0.0,
-		"history":
-		[
-		]
-	},
-	"distraction_free":
-	{
-		"menu_visible": true,
-		"show_minimap": false,
-		"show_open_files": false,
-		"show_tabs": false,
-		"side_bar_visible": false,
-		"status_bar_visible": false
-	},
-	"file_history":
-	[
-		"/C/xampp/htdocs/Imposto de renda/impRenda.php"
-	],
-	"find":
-	{
-		"height": 0.0
-	},
-	"find_in_files":
-	{
-		"height": 0.0,
-		"where_history":
-		[
-		]
-	},
-	"find_state":
-	{
-		"case_sensitive": false,
-		"find_history":
-		[
-		],
-		"highlight": true,
-		"in_selection": false,
-		"preserve_case": false,
-		"regex": false,
-		"replace_history":
-		[
-		],
-		"reverse": false,
-		"scrollbar_highlights": true,
-		"show_context": true,
-		"use_buffer2": true,
-		"use_gitignore": true,
-		"whole_word": false,
-		"wrap": true
-	},
-	"groups":
-	[
-		{
-			"sheets":
-			[
-				{
-					"buffer": 0,
-					"file": "impRenda.php",
-					"selected": true,
-					"semi_transient": false,
-					"settings":
-					{
-						"buffer_size": 718,
-						"regions":
-						{
-						},
-						"selection":
-						[
-							[
-								718,
-								718
-							]
-						],
-						"settings":
-						{
-							"syntax": "Packages/PHP/PHP.sublime-syntax",
-							"tab_size": 4,
-							"translate_tabs_to_spaces": true
-						},
-						"translation.x": 0.0,
-						"translation.y": 0.0,
-						"zoom_level": 1.0
-					},
-					"stack_index": 0,
-					"stack_multiselect": false,
-					"type": "text"
-				}
-			]
-		}
-	],
-	"incremental_find":
-	{
-		"height": 0.0
-	},
-	"input":
-	{
-		"height": 0.0
-	},
-	"layout":
-	{
-		"cells":
-		[
-			[
-				0,
-				0,
-				1,
-				1
-			]
-		],
-		"cols":
-		[
-			0.0,
-			1.0
-		],
-		"rows":
-		[
-			0.0,
-			1.0
-		]
-	},
-	"menu_visible": true,
-	"output.find_results":
-	{
-		"height": 0.0
-	},
-	"pinned_build_system": "",
-	"project": "impRenda.php",
-	"replace":
-	{
-		"height": 0.0
-	},
-	"save_all_on_build": true,
-	"select_file":
-	{
-		"height": 0.0,
-		"last_filter": "",
-		"selected_items":
-		[
-		],
-		"width": 0.0
-	},
-	"select_project":
-	{
-		"height": 0.0,
-		"last_filter": "",
-		"selected_items":
-		[
-		],
-		"width": 0.0
-	},
-	"select_symbol":
-	{
-		"height": 0.0,
-		"last_filter": "",
-		"selected_items":
-		[
-		],
-		"width": 0.0
-	},
-	"selected_group": 0,
-	"settings":
-	{
-	},
-	"show_minimap": true,
-	"show_open_files": false,
-	"show_tabs": true,
-	"side_bar_visible": true,
-	"side_bar_width": 150.0,
-	"status_bar_visible": true,
-	"template_settings":
-	{
-	}
+<?php
+ 
+$salarioBruto = 7500.00;
+ 
+ 
+if ($salarioBruto <= 3036.00) {
+    $aliquota = 0;
+} elseif ($salarioBruto <= 3533.31) {
+    $aliquota = 7.5;
+} elseif ($salarioBruto <= 4688.85) {
+    $aliquota = 15;
+} elseif ($salarioBruto <= 5830.85) {
+    $aliquota = 22.5;
+} else {
+    $aliquota = 27.5;
 }
+ 
+$valorDesconto = ($salarioBruto * $aliquota) / 100;
+ 
+$salarioLiquido = $salarioBruto - $valorDesconto;
+ 
+echo "Salário bruto: R$ " . number_format($salarioBruto, 2, ',', '.') . "<br>";
+echo "Porcentagem de desconto: " . $aliquota . "%<br>";
+echo "Valor do desconto: R$ " . number_format($valorDesconto, 2, ',', '.') . "<br>";
+echo "Salário líquido a receber: R$ " . number_format($salarioLiquido, 2, ',', '.');
+?>
+ 
